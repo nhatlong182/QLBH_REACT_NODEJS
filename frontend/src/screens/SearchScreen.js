be00a4f1a39c8cb969004c998e5cde6b0fb4a816
kipper <tuilaahihi@gmail.com>
2021-09-15 14:39:34 +0700

    add view loc san pham

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../actions/productAction.js';
 import { Link, useParams } from 'react-router-dom';
@@ -6,14 +6,16 @@ import { Link, useParams } from 'react-router-dom';
 import Product from '../components/Product.js'
 import LoadingBox from '../components/LoadingBox.js'
 import MessageBox from '../components/MessageBox.js'
+import  '../css/search.css';
 
 export default function SearchScreen() {
+    const [filterIsOpen, setfilterIsOpen] = useState(false);
     const { pageNumber = 1, limit = 12, name = '', category = '' } = useParams();
 
     const productList = useSelector((state) => state.productList);
     const { loading, error, products, page, pages } = productList;
     const dispatch = useDispatch();
-
+ 
 
     const getFilterUrl = (filter) => {
         const filterPage = filter.page || pageNumber;
@@ -33,9 +35,30 @@ export default function SearchScreen() {
 
     return (
         <div>
-
+            <div className="shop-sidebar">
+            <div className="filterbtn">
+            <span onClick={() => setfilterIsOpen(!filterIsOpen)} className="btn-filter" >Lọc</span>
+            </div>
+            <div className={filterIsOpen ? 'filter' : ''} id="filterbar">
+               <div className="filter-content">
+                    <ul className="filter-data">
+                        <h3 className="text-sortby">Sort By</h3>
+                        <li>Giá: Từ Thấp đến Cao</li>
+                        <li>Giá: Từ Cao đến Thấp</li>
+                    </ul>
+                    <ul className="filter-data" >
+                        <h3 className="text-sortby">Giá</h3>
+                        <li>0 - 200,000₫</li>
+                        <li>200,000₫ - 400,000₫</li>
+                        <li>400,000₫ - 600,000₫</li>
+                        <li>600,000₫ - 800,000₫</li>
+                        <li>800,000₫ - 1,000,000₫</li>
+                    </ul>
+               </div>
+            </div>
+            </div>
             {loading ? <LoadingBox></LoadingBox> : error ? <MessageBox variant="danger">{error}</MessageBox> :
-                <div className="row center">
+                <div className="popular_container">
                     {
                         products?.map(product => (
                             <Product key={product._id} product={product}></Product>
