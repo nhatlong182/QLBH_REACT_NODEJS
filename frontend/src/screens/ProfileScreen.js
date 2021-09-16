3bec39e2e896ddfe0b676039d0f938d7a32d9fc9
nhatlong182 <nhatlong1826@gmail.com>
2021-09-16 19:44:30 +0700

    Them profile

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,3 +1,4 @@
+import { set } from 'mongoose';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { detailsUser } from '../actions/userAction.js';
@@ -9,69 +10,89 @@ import '../css/profile.css'
 
 
 export default function ProfileScreen() {
-    const [name, setName] = useState('');
-    // eslint-disable-next-line
-    const [password, setPassword] = useState('');
-    // eslint-disable-next-line
-    const [confirmPassword, setConfirmPassword] = useState('');
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [newPassword, setNewPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [phone, setPhone] = useState('');
+	const [avatar, setAvatar] = useState('');
 
-    const userSignin = useSelector((state) => state.userSignin);
-    const { userInfo } = userSignin;
-    const userDetails = useSelector((state) => state.userDetail);
-    const { loading, error, user } = userDetails;
+	const userSignin = useSelector((state) => state.userSignin);
+	const { userInfo } = userSignin;
+	const userDetails = useSelector((state) => state.userDetail);
+	const { loading, error, user } = userDetails;
 
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (!user) {
-            dispatch(detailsUser(userInfo._id));
-        }
-    }, [dispatch, userInfo._id, user]);
+	useEffect(() => {
+		if (!user) {
+			dispatch(detailsUser(userInfo._id));
+		}
+		else {
+			setName(user.name);
+			setEmail(user.email);
+			user.phone ? setPhone(user.phone) : setPhone('');
+			setAvatar(user.avatar);
+		}
+	}, [dispatch, userInfo._id, user]);
 
-    return (
-			
-        <div className="content-pf" >
-        <div class="container-fluid">
-			<div className="pf-row">
-				<div className="text_acc"><h3>Tài khoản</h3></div>
-			</div>
-		<div class="pf-row">
-			<div class="pf_text"><label for="account_last_name">Họ* </label></div>
-			<div class="pf_box"><input type="text"/></div>
-		</div>
+	const submitHandler = (e) => {
+		e.preventDefault();
 
-		<div class="pf-row">
-			<div class="pf_text"><label for="account_display_name">Tên hiển thị* </label></div>
-			<div class="pf_box"><input type="text"/> </div>
-		</div>
+		console.log(name, email, phone, avatar);
+	}
 
-		<div class="pf-row">
-			<div class="pf_text"><label for="account_email">Địa chỉ email* </label></div>
-			<div class="pf_box"><input type="email" /></div>
-		</div>
-	</div>
+	return (
+		<div>
+			<form className="" onSubmit={submitHandler}>
+				{loading ? (
+					<LoadingBox></LoadingBox>
+				) : error ? (
+					<MessageBox variant="danger">{error}</MessageBox>
+				) : (
+					<div className="content-pf" >
+						<div className="container-fluid">
+							<img src={avatar} alt="profile-avatar" className=""></img>
 
-	<div class="container-fluid">
-		<div class="pf-row">
-			<div class="text_pass"><h3>Thay đổi mật khẩu</h3></div>
-		</div>	
-		<div class="pf-row">
-			<div class="pf_text"><label for="password_current">Mật khẩu hiện tại (bỏ trống nếu không đổi)</label></div>
-			<div class="pf_box"><input type="password" autocomplete="off" /></div>
+							<div className="pf-row">
+								<div className="pf_text"><label htmlFor="name">Tên hiển thị* </label></div>
+								<div className="pf_box">
+									<input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} disabled />
+								</div>
+							</div>
+							<div className="pf-row">
+								<div className="pf_text"><label htmlFor="phone">Số điện thoại* </label></div>
+								<div className="pf_box">
+									<input id="phone" type="text" value={phone} onChange={(e) => setPhone(e.target.value)} disabled />
+								</div>
+							</div>
+						</div>
+
+						<div className="container-fluid">
+							<div className="pf-row">
+								<div className="text_pass"><h3>Thay đổi mật khẩu</h3></div>
+							</div>
+							<div className="pf-row">
+								<div className="pf_text"><label htmlFor="password_current">Mật khẩu hiện tại (bỏ trống nếu không đổi)</label></div>
+								<div className="pf_box"><input id="password_current" type="password" onChange={(e) => setPassword(e.target.value)} /></div>
+							</div>
+							<div className="pf-row">
+								<div className="pf_text"><label htmlFor="password_new">Mật khẩu mới (bỏ trống nếu không đổi)</label></div>
+								<div className="pf_box"><input id="password_new" type="password" onChange={(e) => setNewPassword(e.target.value)} /></div>
+							</div>
+							<div className="pf-row">
+								<div className="pf_text"><label htmlFor="password_new_confirm">Xác nhận mật khẩu mới</label></div>
+								<div className="pf_box"><input id="password_new_confirm" type="password" onChange={(e) => setConfirmPassword(e.target.value)} /></div>
+							</div>
+
+						</div>
+						<div className="pf_btn">
+							<button type="submit" className="btn-submit">Lưu thay đổi</button>
+						</div>
+					</div>
+				)}
+			</form>
 		</div>
-		<div class="pf-row">
-			<div class="pf_text"><label for="password_1">Mật khẩu mới (bỏ trống nếu không đổi)</label></div>
-			<div class="pf_box"><input type="password" autocomplete="off" /></div>
-		</div>
-		<div class="pf-row">
-			<div class="pf_text"><label for="password_2">Xác nhận mật khẩu mới</label></div>
-			<div class="pf_box"><input type="password" autocomplete="off" /></div>
-		</div>
-		
-        </div>
-			<div class="pf_btn">
-				<button type="submit" class="btn-submit">Lưu thay đổi</button>
-		</div>
-        </div>
-    )
+	)
 }
